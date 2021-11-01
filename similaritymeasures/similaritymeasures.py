70935dcb605b5993fa8fdb7ccf535e2f5e67e95f
tinyint <tinyint@163.com>
2021-11-01 19:55:59 +0800

    add ddtw

--- similaritymeasures/similaritymeasures.py
+++ similaritymeasures/similaritymeasures.py
@@ -888,6 +888,218 @@ def dtw_path(d):
     return path[::-1]
 
 
+def ddtw(exp_data, num_data, metric='euclidean', **kwargs):
+    r"""
+    Compute the Dynamic Time Warping distance.
+
+    This computes a generic Dynamic Time Warping (DTW) distance and follows
+    the algorithm from [1]_. This can use all distance metrics that are
+    available in scipy.spatial.distance.cdist.
+
+    Parameters
+    ----------
+    exp_data : array_like
+        Curve from your experimental data. exp_data is of (M, N) shape, where
+        M is the number of data points, and N is the number of dimmensions
+    num_data : array_like
+        Curve from your numerical data. num_data is of (P, N) shape, where P
+        is the number of data points, and N is the number of dimmensions
+    metric : str or callable, optional
+        The distance metric to use. Default='euclidean'. Refer to the
+        documentation for scipy.spatial.distance.cdist. Some examples:
+        'braycurtis', 'canberra', 'chebyshev', 'cityblock', 'correlation',
+        'cosine', 'dice', 'euclidean', 'hamming', 'jaccard', 'kulsinski',
+        'mahalanobis', 'matching', 'minkowski', 'rogerstanimoto', 'russellrao',
+        'seuclidean', 'sokalmichener', 'sokalsneath', 'sqeuclidean',
+        'wminkowski', 'yule'.
+    **kwargs : dict, optional
+        Extra arguments to `metric`: refer to each metric documentation in
+        scipy.spatial.distance.
+
+        Some examples:
+
+        p : scalar
+            The p-norm to apply for Minkowski, weighted and unweighted.
+            Default: 2.
+
+        w : ndarray
+            The weight vector for metrics that support weights (e.g.,
+            Minkowski).
+
+        V : ndarray
+            The variance vector for standardized Euclidean.
+            Default: var(vstack([XA, XB]), axis=0, ddof=1)
+
+        VI : ndarray
+            The inverse of the covariance matrix for Mahalanobis.
+            Default: inv(cov(vstack([XA, XB].T))).T
+
+        out : ndarray
+            The output array
+            If not None, the distance matrix Y is stored in this array.
+
+    Retruns
+    -------
+    r : float
+        DTW distance.
+    d : ndarray (2-D)
+        Cumulative distance matrix
+
+    Notes
+    -----
+    The DTW distance is d[-1, -1].
+
+    This has O(M, P) computational cost.
+
+    The latest scipy.spatial.distance.cdist information can be found at
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.cdist.html
+
+    Your x locations of data points should be exp_data[:, 0], and the y
+    locations of the data points should be exp_data[:, 1]. Same for num_data.
+
+    This uses the euclidean distance for now. In the future it should be
+    possible to support other metrics.
+
+    DTW is a non-metric distance, which means DTW doesn't hold the triangle
+    inequality.
+    https://en.wikipedia.org/wiki/Triangle_inequality
+
+    References
+    ----------
+    .. [1] Senin, P., 2008. Dynamic time warping algorithm review. Information
+        and Computer Science Department University of Hawaii at Manoa Honolulu,
+        USA, 855, pp.1-23.
+        http://seninp.github.io/assets/pubs/senin_dtw_litreview_2008.pdf
+
+    Examples
+    --------
+    >>> # Generate random experimental data
+    >>> x = np.random.random(100)
+    >>> y = np.random.random(100)
+    >>> exp_data = np.zeros((100, 2))
+    >>> exp_data[:, 0] = x
+    >>> exp_data[:, 1] = y
+    >>> # Generate random numerical data
+    >>> x = np.random.random(100)
+    >>> y = np.random.random(100)
+    >>> num_data = np.zeros((100, 2))
+    >>> num_data[:, 0] = x
+    >>> num_data[:, 1] = y
+    >>> r, d = dtw(exp_data, num_data)
+
+    The euclidean distance is used by default. You can use metric and **kwargs
+    to specify different types of distance metrics. The following example uses
+    the city block or Manhattan distance between points.
+
+    >>> r, d = dtw(exp_data, num_data, metric='cityblock')
+
+    """
+    c = distance.cdist(exp_data, num_data, metric=metric, **kwargs)
+
+    d = np.zeros(c.shape)
+    d[0, 0] = c[0, 0]
+    n, m = c.shape
+    for i in range(1, n):
+        d[i, 0] = d[i-1, 0] + c[i, 0]
+    for j in range(1, m):
+        d[0, j] = d[0, j-1] + c[0, j]
+    for i in range(1, n):
+        for j in range(1, m):
+            d[i, j] = c[i, j] + min((d[i-1, j], d[i, j-1], d[i-1, j-1]))
+    return d[-1, -1], d
+
+
+def ddtw_path(d):
+    r"""
+    Calculates the optimal DTW path from a given DTW cumulative distance
+    matrix.
+
+    This function returns the optimal DTW path using the back propagation
+    algorithm that is defined in [1]_. This path details the index from each
+    curve that is being compared.
+
+    Parameters
+    ----------
+    d : ndarray (2-D)
+        Cumulative distance matrix.
+
+    Returns
+    -------
+    path : ndarray (2-D)
+        The optimal DTW path.
+
+    Notes
+    -----
+    Note that path[:, 0] represents the indices from exp_data, while
+    path[:, 1] represents the indices from the num_data.
+
+    References
+    ----------
+    .. [1] Senin, P., 2008. Dynamic time warping algorithm review. Information
+        and Computer Science Department University of Hawaii at Manoa Honolulu,
+        USA, 855, pp.1-23.
+        http://seninp.github.io/assets/pubs/senin_dtw_litreview_2008.pdf
+
+    Examples
+    --------
+    First calculate the DTW cumulative distance matrix.
+
+    >>> # Generate random experimental data
+    >>> x = np.random.random(100)
+    >>> y = np.random.random(100)
+    >>> exp_data = np.zeros((100, 2))
+    >>> exp_data[:, 0] = x
+    >>> exp_data[:, 1] = y
+    >>> # Generate random numerical data
+    >>> x = np.random.random(100)
+    >>> y = np.random.random(100)
+    >>> num_data = np.zeros((100, 2))
+    >>> num_data[:, 0] = x
+    >>> num_data[:, 1] = y
+    >>> r, d = dtw(exp_data, num_data)
+
+    Now you can calculate the optimal DTW path
+
+    >>> path = dtw_path(d)
+
+    You can visualize the path on the cumulative distance matrix using the
+    following code.
+
+    >>> import matplotlib.pyplot as plt
+    >>> plt.figure()
+    >>> plt.imshow(d.T, origin='lower')
+    >>> plt.plot(path[:, 0], path[:, 1], '-k')
+    >>> plt.colorbar()
+    >>> plt.show()
+
+    """
+    path = []
+    i, j = d.shape
+    i = i - 1
+    j = j - 1
+    # back propagation starts from the last point,
+    # and ends at d[0, 0]
+    path.append((i, j))
+    while i > 0 or j > 0:
+        if i == 0:
+            j = j - 1
+        elif j == 0:
+            i = i - 1
+        else:
+            temp_step = min([d[i-1, j], d[i, j-1], d[i-1, j-1]])
+            if d[i-1, j] == temp_step:
+                i = i - 1
+            elif d[i, j-1] == temp_step:
+                j = j - 1
+            else:
+                i = i - 1
+                j = j - 1
+        path.append((i, j))
+    path = np.array(path)
+    # reverse the order of path, such that it starts with [0, 0]
+    return path[::-1]
+
+
 def mae(exp_data, num_data):
     """
     Compute the Mean Absolute Error (MAE).
